20ac8a522dc7d58a8e00d980a2c0590d67ef117a
amayas mahmoudi <amayasmah562@gmail.com>
2024-02-27 21:59:50 +0100

    Main file + requirements file

---
+++ Main.py
@@ -0,0 +1,11 @@
+import logging
+
+
+
+logging.basicConfig(level=logging.INFO, filename="./Log/SupTrading.log", filemode="a", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+
+
+
